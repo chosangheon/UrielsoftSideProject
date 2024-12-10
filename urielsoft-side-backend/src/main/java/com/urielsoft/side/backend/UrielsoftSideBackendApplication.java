32f3e080d3c0ccb43a30856c1ce3d27dd15f2f85
shcho <shcho@urielsoft.co.kr>
2024-12-10 18:32:48 +0900

    - DB 구성 후 테스트 재 진행. - 백엔드 커밋

---
+++ urielsoft-side-backend/src/main/java/com/urielsoft/side/backend/UrielsoftSideBackendApplication.java
@@ -0,0 +1,13 @@
+package com.urielsoft.side.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrielsoftSideBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrielsoftSideBackendApplication.class, args);
+    }
+
+}
